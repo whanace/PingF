0079b12fcc5a2103f1c7059da31ddd4f9b4570fc
석중환 <whanace@gmail.com>
2015-08-23 18:53:31 +0900

    layout 설정

--- app/src/main/java/xyz/pingtest/pingf/SampleMain.java
+++ app/src/main/java/xyz/pingtest/pingf/SampleMain.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-
 import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Color;
